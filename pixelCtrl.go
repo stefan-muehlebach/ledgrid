c27750d76d56b603cbc5cd1e39c81b0de4a97aaf
Stefan Mühlebach <stefan.muehlebach@gmx.net>
2024-08-03 23:44:32 +0200

    'LiteIDE'

--- pixelCtrl.go
+++ pixelCtrl.go
@@ -328,7 +328,7 @@ func (p *PixelServer) ToggleTestPattern() bool {
             }
 
 			p.Disp.Send(p.buffer[:TestBufferSize])
-			// time.Sleep(80 * time.Millisecond)
+			time.Sleep(500 * time.Millisecond)
 		}
 		for i := range TestBufferSize {
 			p.buffer[i] = 0x00
