b1f79810c0a13e30c0a639d74cd637b0b3bc29ee
Stefan Mühlebach <stefan.muehlebach@gmx.net>
2024-09-03 23:30:36 +0200

    'LiteIDE'

---
+++ gridClient.go
@@ -0,0 +1,171 @@
+package ledgrid
+
+import (
+	"fmt"
+	"image"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+// Um den clientseitigen Code so generisch wie moeglich zu halten, ist der
+// GridClient als Interface definiert. Zwei konkrete Implementationen
+// stehen zur Verfuegung:
+// - NetGridClient
+// - DummyGridClient
+type GridClient interface {
+	Send(lg *LedGrid)
+	Size() image.Point
+	Gamma() (r, g, b float64)
+	SetGamma(r, g, b float64)
+	MaxBright() (r, g, b uint8)
+	SetMaxBright(r, g, b uint8)
+	Watch() *Stopwatch
+	Close()
+}
+
+// Mit diesem Typ wird die klassische Verwendung auf zwei Nodes realisiert.
+type NetGridClient struct {
+	addr      *net.UDPAddr
+	conn      *net.UDPConn
+	rpcClient *rpc.Client
+	sendWatch *Stopwatch
+}
+
+func NewNetGridClient(host string, port uint) GridClient {
+	var hostPort string
+	var err error
+
+	p := &NetGridClient{}
+	hostPort = fmt.Sprintf("%s:%d", host, port)
+	p.addr, err = net.ResolveUDPAddr("udp", hostPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.conn, err = net.DialUDP("udp", nil, p.addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.rpcClient, err = rpc.DialHTTP("tcp", hostPort)
+	if err != nil {
+		log.Fatal("Dialing:", err)
+	}
+	p.sendWatch = NewStopwatch()
+
+	return p
+}
+
+// Sendet die Bilddaten in der LedGrid-Struktur zum Controller.
+func (p *NetGridClient) Send(lg *LedGrid) {
+	var err error
+
+	p.sendWatch.Start()
+	_, err = p.conn.Write(lg.Pix)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.sendWatch.Stop()
+}
+
+func (p *NetGridClient) Size() (size image.Point) {
+	var reply SizeArg
+	var err error
+
+	err = p.rpcClient.Call("GridServer.Size", 0, &reply)
+	if err != nil {
+		log.Fatal("Size error:", err)
+	}
+	return image.Point(reply)
+}
+
+func (p *NetGridClient) Gamma() (r, g, b float64) {
+	var reply GammaArg
+	var err error
+
+	err = p.rpcClient.Call("GridServer.RPCGamma", 0, &reply)
+	if err != nil {
+		log.Fatal("Gamma error:", err)
+	}
+	return reply.RedVal, reply.GreenVal, reply.BlueVal
+}
+
+func (p *NetGridClient) SetGamma(r, g, b float64) {
+	var reply int
+	var err error
+
+	err = p.rpcClient.Call("GridServer.RPCSetGamma", GammaArg{r, g, b}, &reply)
+	if err != nil {
+		log.Fatal("SetGamma error:", err)
+	}
+}
+
+func (p *NetGridClient) MaxBright() (r, g, b uint8) {
+	var reply BrightArg
+	var err error
+
+	err = p.rpcClient.Call("GridServer.RPCMaxBright", 0, &reply)
+	if err != nil {
+		log.Fatal("MaxBright error:", err)
+	}
+	return reply.RedVal, reply.GreenVal, reply.BlueVal
+}
+
+func (p *NetGridClient) SetMaxBright(r, g, b uint8) {
+	var reply int
+	var err error
+
+	err = p.rpcClient.Call("GridServer.RPCSetMaxBright", BrightArg{r, g, b}, &reply)
+	if err != nil {
+		log.Fatal("SetMaxBright error:", err)
+	}
+}
+
+func (p *NetGridClient) Watch() *Stopwatch {
+	return p.sendWatch
+}
+
+// Schliesst die Verbindung zum Controller.
+func (p *NetGridClient) Close() {
+	p.conn.Close()
+}
+
+// Mit dieser Implementation des GridClient-Interfaces kann man ohne Zugriff
+// auf ein reales LED-Grid Software testen.
+type DummyGridClient struct {
+	size image.Point
+}
+
+func NewDummyGridClient(size image.Point) GridClient {
+	p := &DummyGridClient{size: size}
+	return p
+}
+
+func (p *DummyGridClient) Send(lg *LedGrid) { }
+
+func (p *DummyGridClient) Size() image.Point {
+	return p.size
+}
+
+func (p *DummyGridClient) Gamma() (r, g, b float64) {
+	return 1.0, 1.0, 1.0
+}
+
+func (p *DummyGridClient) SetGamma(r, g, b float64) {}
+
+func (p *DummyGridClient) MaxBright() (r, g, b uint8) {
+	return 0xff, 0xff, 0xff
+}
+
+func (p *DummyGridClient) SetMaxBright(r, g, b uint8) {}
+
+func (p *DummyGridClient) Watch() *Stopwatch {
+    // TO DO: even a dummy implementation of the client should return a
+    // usable Stopwatch. Otherwise, the calling function may crash if we just
+    // return nil...
+	return nil
+}
+
+func (p *DummyGridClient) Close() {}
+
+
---
+++ gridServer.go
@@ -0,0 +1,339 @@
+package ledgrid
+
+import (
+	"errors"
+	"image"
+	"log"
+	"math"
+	"net"
+	"net/http"
+	"net/netip"
+	"net/rpc"
+	"time"
+)
+
+const (
+// Dies ist die Groesse des Buffers, welcher fuer den Empfang der
+// LED-Daten zur Verfuegung steht. Er ist bewusst extrem grosszuegig
+// dimensioniert... ;-)
+// bufferSize = 320 * 240 * 3
+)
+
+type PixelStatusType byte
+
+const (
+	PixelOK PixelStatusType = iota
+	PixelMissing
+	PixelDefect
+)
+
+// Der GridServer wird auf jenem Geraet gestartet, an dem das LedGrid via
+// SPI angeschlossen ist oder allenfalls der Emulator laeuft.
+type GridServer struct {
+	Disp                 Displayer
+	udpAddr              *net.UDPAddr
+	udpConn              *net.UDPConn
+	tcpAddr              *net.TCPAddr
+	tcpListener          *net.TCPListener
+	buffer               []byte
+	statusList           []PixelStatusType
+	gammaValue           [3]float64
+	maxValue             [3]uint8
+	gamma                [3][256]byte
+	drawTestPattern      bool
+	sendWatch            *Stopwatch
+	RecvBytes, SentBytes int
+}
+
+// Damit wird eine neue Instanz eines GridServers erzeugt. Mit port wird
+// sowohl die UDP- als auch die TCP-Portnummer bezeichnet. spiDev enthaelt
+// das Device-File des SPI-Anschlusses und mit baud wird die Geschwindigkeit
+// des SPI-Interfaces in Baud bezeichnet.
+func NewGridServer(port uint, disp Displayer) *GridServer {
+	var err error
+	var addrPort netip.AddrPort
+	var bufferSize int
+
+	p := &GridServer{Disp: disp}
+	bufferSize = 3 * disp.Size().X * disp.Size().Y
+	// Dann erstellen wir einen Buffer fuer die via Netzwerk eintreffenden
+	// Daten und initialisieren, die Slices fuer die fehlenden (d.h. aus
+	// der LED-Kette entfernten) und die fehlerhaften (d.h. die LEDs, welche
+	// als Farbe immer Schwarz erhalten sollen).
+	p.buffer = make([]byte, bufferSize)
+	p.statusList = make([]PixelStatusType, bufferSize/3)
+
+	// Anschliessend werden die Tabellen fuer die Farbwertkorrektur und die
+	// maximale Helligkeit erstellt.
+	p.gammaValue[0], p.gammaValue[1], p.gammaValue[2] = p.Disp.DefaultGamma()
+	p.maxValue = [3]uint8{255, 255, 255}
+	p.updateGammaTable()
+
+	p.sendWatch = NewStopwatch()
+
+	// Jetzt wird der UDP-Port geoeffnet, resp. eine lesende Verbindung
+	// dafuer erstellt.
+	addrPort = netip.AddrPortFrom(netip.IPv4Unspecified(), uint16(port))
+	if !addrPort.IsValid() {
+		log.Fatalf("Invalid address or port")
+	}
+	p.udpAddr = net.UDPAddrFromAddrPort(addrPort)
+	p.udpConn, err = net.ListenUDP("udp", p.udpAddr)
+	if err != nil {
+		log.Fatal("UDP listen error:", err)
+	}
+
+	// Anschliessend wird die RPC-Verbindung initiiert.
+	rpc.Register(p)
+	rpc.HandleHTTP()
+	p.tcpAddr = net.TCPAddrFromAddrPort(addrPort)
+	p.tcpListener, err = net.ListenTCP("tcp", p.tcpAddr)
+	if err != nil {
+		log.Fatal("TCP listen error:", err)
+	}
+	go http.Serve(p.tcpListener, nil)
+
+	return p
+}
+
+// Schliesst die diversen Verbindungen.
+func (p *GridServer) Close() {
+	p.udpConn.Close()
+	p.tcpListener.Close()
+}
+
+func (p *GridServer) Watch() *Stopwatch {
+	return p.sendWatch
+}
+
+// Retourniert die Gamma-Werte fuer die drei Farben.
+func (p *GridServer) Gamma() (r, g, b float64) {
+	return p.gammaValue[0], p.gammaValue[1], p.gammaValue[2]
+}
+
+// Setzt die Gamma-Werte fuer die Farben und aktualisiert die Mapping-Tabelle.
+func (p *GridServer) SetGamma(r, g, b float64) {
+	p.gammaValue[0], p.gammaValue[1], p.gammaValue[2] = r, g, b
+	p.updateGammaTable()
+}
+
+// Setzt pro Farbe den maximal erlaubten Farbwert als uint8-Wert
+func (p *GridServer) MaxBright() (r, g, b uint8) {
+	return p.maxValue[0], p.maxValue[1], p.maxValue[2]
+}
+
+func (p *GridServer) SetMaxBright(r, g, b uint8) {
+	p.maxValue[0], p.maxValue[1], p.maxValue[2] = r, g, b
+	p.updateGammaTable()
+}
+
+func (p *GridServer) SetPixelStatus(idx int, stat PixelStatusType) {
+	p.statusList[idx] = stat
+}
+
+func (p *GridServer) updateGammaTable() {
+	for color, val := range p.gammaValue {
+		max := float64(p.maxValue[color])
+		for i := range 256 {
+			p.gamma[color][i] = byte(max * math.Pow(float64(i)/255.0, val))
+		}
+	}
+}
+
+const (
+	TestRed = iota
+	TestGreen
+	TestBlue
+	TestYellow
+	TestMagenta
+	TestCyan
+	TestWhite
+	NumColorModes
+)
+
+func (p *GridServer) ToggleTestPattern() bool {
+	var colorMode int
+	var colorValue byte
+
+	var numTestLeds = p.Disp.Size().X * p.Disp.Size().Y
+	var testBufferSize = 3 * numTestLeds
+
+	if p.drawTestPattern {
+		p.drawTestPattern = false
+		return false
+	} else {
+		p.drawTestPattern = true
+		colorMode = TestRed
+	}
+
+	go func() {
+		colorValue = 0x00
+		for p.drawTestPattern {
+			switch colorMode {
+			case TestRed:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = colorValue
+					p.buffer[3*i+1] = 0x00
+					p.buffer[3*i+2] = 0x00
+				}
+			case TestGreen:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = 0x00
+					p.buffer[3*i+1] = colorValue
+					p.buffer[3*i+2] = 0x00
+				}
+			case TestBlue:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = 0x00
+					p.buffer[3*i+1] = 0x00
+					p.buffer[3*i+2] = colorValue
+				}
+			case TestYellow:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = colorValue
+					p.buffer[3*i+1] = colorValue
+					p.buffer[3*i+2] = 0x00
+				}
+			case TestMagenta:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = colorValue
+					p.buffer[3*i+1] = 0x00
+					p.buffer[3*i+2] = colorValue
+				}
+			case TestCyan:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = 0x00
+					p.buffer[3*i+1] = colorValue
+					p.buffer[3*i+2] = colorValue
+				}
+			case TestWhite:
+				for i := range numTestLeds {
+					p.buffer[3*i+0] = colorValue
+					p.buffer[3*i+1] = colorValue
+					p.buffer[3*i+2] = colorValue
+				}
+			}
+
+			if colorValue < 0xff {
+				colorValue = (colorValue << 1) | 1
+			} else {
+				colorValue = 0x00
+				colorMode = (colorMode + 1) % NumColorModes
+			}
+			p.sendWatch.Start()
+			p.Disp.Send(p.buffer[:testBufferSize])
+			p.sendWatch.Stop()
+			time.Sleep(300 * time.Millisecond)
+		}
+		for i := range testBufferSize {
+			p.buffer[i] = 0x00
+		}
+		p.sendWatch.Start()
+		p.Disp.Send(p.buffer)
+		p.sendWatch.Stop()
+	}()
+
+	return true
+}
+
+// Dies ist die zentrale Verarbeitungs-Funktion des Grid-Controllers. In ihr
+// wird laufend ein Datenpaket via UDP empfangen, die empfangenen Werte gem.
+// Gamma-Korrektur umgeschrieben und via SPI-Bus auf das LED-Grid uebertragen.
+// Die genaue Konfiguration des LED-Grids (Anordnung der Lichterketten) ist
+// dem Grid-Controller nicht bekannt.
+func (p *GridServer) Handle() {
+	var bufferSize, numLEDs int
+	var src, dst []byte
+	var err error
+
+	for {
+		bufferSize, err = p.udpConn.Read(p.buffer)
+		if err != nil {
+			if errors.Is(err, net.ErrClosed) {
+				break
+			}
+			log.Fatal(err)
+		}
+		p.RecvBytes += bufferSize
+		p.sendWatch.Start()
+		numLEDs = bufferSize / 3
+		for srcIdx, dstIdx := 0, 0; srcIdx < numLEDs; srcIdx++ {
+			if p.statusList[srcIdx] == PixelMissing {
+				continue
+			}
+			dst = p.buffer[3*dstIdx : 3*dstIdx+3 : 3*dstIdx+3]
+			if p.statusList[srcIdx] == PixelDefect {
+				dst[0] = 0x00
+				dst[1] = 0x00
+				dst[2] = 0x00
+			} else {
+				src = p.buffer[3*srcIdx : 3*srcIdx+3 : 3*srcIdx+3]
+				dst[0] = p.gamma[0][src[0]]
+				dst[1] = p.gamma[1][src[1]]
+				dst[2] = p.gamma[2][src[2]]
+			}
+			dstIdx++
+		}
+		p.Disp.Send(p.buffer[:bufferSize])
+		p.SentBytes += bufferSize
+		p.sendWatch.Stop()
+	}
+
+	// Vor dem Beenden des Programms werden alle LEDs Schwarz geschaltet
+	// damit das Panel dunkel wird.
+	for i := range p.buffer {
+		p.buffer[i] = 0x00
+	}
+	p.Disp.Send(p.buffer)
+	p.SentBytes += len(p.buffer)
+	p.Disp.Close()
+}
+
+// Die folgenden Methoden koennen via RPC vom Client aufgerufen werden.
+// Die Methode RPCDraw ist nur der Vollstaendigkeit halber vorhanden. In
+// der Praxis hat sich das Senden der Bilddaten via RPC als zu langsam
+// erwiesen und wurde auf UDP umgestellt.
+func (p *GridServer) RPCDraw(grid *LedGrid, reply *int) error {
+	var err error
+
+	for i := 0; i < len(grid.Pix); i++ {
+		grid.Pix[i] = p.gamma[i%3][grid.Pix[i]]
+	}
+	p.Disp.Send(grid.Pix)
+	return err
+}
+
+type SizeArg image.Point
+
+func (p *GridServer) RPCSize(arg int, reply *SizeArg) error {
+	*reply = SizeArg(p.Disp.Size())
+	return nil
+}
+
+type GammaArg struct {
+	RedVal, GreenVal, BlueVal float64
+}
+
+func (p *GridServer) RPCSetGamma(arg GammaArg, reply *int) error {
+	p.SetGamma(arg.RedVal, arg.GreenVal, arg.BlueVal)
+	return nil
+}
+
+func (p *GridServer) RPCGamma(arg int, reply *GammaArg) error {
+	reply.RedVal, reply.GreenVal, reply.BlueVal = p.Gamma()
+	return nil
+}
+
+type BrightArg struct {
+	RedVal, GreenVal, BlueVal uint8
+}
+
+func (p *GridServer) RPCSetMaxBright(arg BrightArg, reply *int) error {
+	p.SetMaxBright(arg.RedVal, arg.GreenVal, arg.BlueVal)
+	return nil
+}
+
+func (p *GridServer) RPCMaxBright(arg int, reply *BrightArg) error {
+	reply.RedVal, reply.GreenVal, reply.BlueVal = p.MaxBright()
+	return nil
+}
