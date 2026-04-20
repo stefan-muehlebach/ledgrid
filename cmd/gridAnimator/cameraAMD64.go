e5e3b67e1d28864562d96c2de4af535a7357ee4f
Stefan Muehlebach <stefan.muehlebach@gmx.net>
2026-04-20 03:20:14 +0200

    .

---
+++ cmd/gridAnimator/cameraAMD64.go
@@ -0,0 +1,13 @@
+//go:build amd64
+
+package main
+
+const (
+	camDevName    = "/dev/video2"
+	camDevId      = 0
+	camWidth      = 320
+	camHeight     = 240
+	camFrameRate  = 30
+	camBufferSize = 1
+)
+
---
+++ cmd/gridAnimator/cameraARM64.go
@@ -0,0 +1,13 @@
+//go:build arm64
+
+package main
+
+const (
+	camDevName    = "/dev/video0"
+	camDevId      = 0
+	camWidth      = 320
+	camHeight     = 240
+	camFrameRate  = 30
+	camBufferSize = 1
+)
+
